0b7a4f57673b312ca9992bc090e3d9694a81f444
Unknown <camille.plages@gmail.com>
2019-03-02 01:01:49 -0500

    Add Barman deplacement

--- Assets/Scripts/Class/Barman/Barman.cs
+++ Assets/Scripts/Class/Barman/Barman.cs
@@ -14,6 +14,7 @@ public class Barman : Singleton<Barman>
     private Aspect aspect;
     private GameObject currentCockail;
     private GameObject currentMonster;
+    private int currentIndex = 0; //index of list monsters in front of the player
 
     // Start is called before the first frame update
     protected override void Start ()
@@ -26,7 +27,15 @@ public class Barman : Singleton<Barman>
     {
         updateAspect();
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Move(KeyCode.LeftArrow); NEED monsters list
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Move(KeyCode.RightArrow); NEED monsters list
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             print("Le Barman sert un monstre");
             // TODO set cocktail du monstre
@@ -51,6 +60,30 @@ public class Barman : Singleton<Barman>
         //TODO : change sprite renderer 
     }
 
+    void Move(KeyCode key, List<MonoBehaviour> listMonsters, int sizeList) //TODO : MonoBehaviour -> Monster
+    {
+        //Go Left
+        if (key == KeyCode.LeftArrow)
+        {
+            int nextIndex = currentIndex - 1;
+            if (nextIndex > 0)
+            {
+                currentIndex = nextIndex;
+            }
+        }
+        //Go Right
+        else if (key == KeyCode.RightArrow)
+        {
+            int nextIndex = currentIndex + 1;
+            if (nextIndex < sizeList)
+            {
+                currentIndex = nextIndex;
+            }
+        }
+        //else : already at the most left or right position 
+        MonoBehaviour currentMonster = listMonsters[currentIndex];
+        transform.position = currentMonster.transform.position;
+    }
 
 
 } 
