d08ae1110514e8ac379e733272967c238d61f174
Unknown <leo.le.corre@gmail.com>
2019-03-02 02:45:43 -0500

    timer global/ monstres et dessins symbol

---
+++ Assets/GlobalTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GlobalTimer : MonoBehaviour
+{
+    public Transform minuteHand;
+    public Transform hourHand;
+    [Range(0f,500f)]
+    public float speed = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        minuteHand.Rotate(Vector3.forward*-speed*Time.deltaTime);
+        hourHand.Rotate(Vector3.forward*(-speed/12f)*Time.deltaTime);
+        
+    }
+}
