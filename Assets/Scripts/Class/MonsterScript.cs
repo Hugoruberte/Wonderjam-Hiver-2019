26332a1f3544e2c44c3c5865d0eb74d1cb3539a9
Luc Boissieras <luc.boissieras@gmail.com>
2019-03-02 09:37:31 +0100

    begin tolerance and monster

--- Assets/Scripts/Class/MonsterScript.cs
+++ Assets/Scripts/Class/MonsterScript.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Interactive.Engine;
 
 public class MonsterScript : MonoBehaviour
 {
+    //monster variables
     public int position;
-    public float waiting = 2.0f;
     public int monsterAspect;
-    
+
+    //time variables
+    public float waiting = 2.0f;
+    private WaitForSeconds waitBeforeLeaving;
+    private float startWaitingTime;
+    private float timeCoeff = 3;
+    private float timeLimit = 0.2f;
+
+    //
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startWaitingTime = Time.time;
+        waitBeforeLeaving = new WaitForSeconds(waiting);
+        StartCoroutine(Leaving());
+
     }
 
     // Update is called once per frame
@@ -21,13 +37,31 @@ public class MonsterScript : MonoBehaviour
         
     }
 
+    IEnumerator Leaving()
+    {
+        yield return waitBeforeLeaving;
+        MonsterManager.instance.LeavingMonster(position);
+    }
     void OnWaitingEnd()
     {
         MonsterManager.instance.TimerEnd(position);
     }
 
-    void SetCocktail(Cocktail)
+    void SetCocktail(ChemicalElementEntity Cocktail)
     {
+        //time Points
+        float currentWaitingTime = Time.time;
+        //actual waiting Time
+        float waitingTime = currentWaitingTime - startWaitingTime;
+
+        //tolerance points lost from time
+        float toleranceTimePoints = ((waitingTime/waiting) - timeLimit) * timeCoeff;
+
+        //color Points
+        Cocktail.att
+
         // Appelez la fonction de Luc de satisfaction et renvoyez au MonsterManager la valeur a ajouter à la satisfaction globale
     }
+
+
 }
---
+++ Assets/Scripts/Class/ToleranceManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToleranceManager : Singleton<ToleranceManager>
+{
+    private float toleranceGaugeMin = 0;
+    private float toleranceGaugeMax = 100;
+    private float toleranceGaugeCurrent = 50;
+
+    void UpdateGaugeValue(float value)
+    {
+        toleranceGaugeCurrent += value;
+        if(toleranceGaugeCurrent <= toleranceGaugeMin)
+        {
+            // YOU LOSE
+        }
+        else if(toleranceGaugeCurrent >= toleranceGaugeMax)
+        {
+            // YOU GOOD I DONT EVEN KNOW WHY THIS CONDITION EXIST
+        }
+    }
+
+
+}
