0eb8498d0922796df7022223bf1fc3f65b3789e2
Loïc <ljedi007@gmail.com>
2019-03-02 01:01:42 -0500

    Add MonsterScript and MonsterManager

---
+++ Assets/Scripts/Class/MonsterManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class MonsterManager : Singleton<MonsterManager>
+{
+    private MonsterScript[] monstres = new MonsterScript[10];
+    public GameObject prefabMonster;
+ 
+    
+    private int[] freeIndice = new int[10] {0,1,2,3,4,5,6,7,8,9}; 
+    
+    
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (freeIndice.Length !=0)
+        {
+            CreateMonster();
+        }
+    }
+
+    void CreateMonster()
+    {
+        int indiceTemp = UnityEngine.Random.Range(0, freeIndice.Length - 1);
+        GameObject MonsterCreate = Instantiate(prefabMonster);
+        MonsterCreate.GetComponent<MonsterScript>().Position = UnityEngine.Random.Range(GetRangeMin(indiceTemp),GetRangeMax(indiceTemp));
+        // TODO : rajoutez un MonsterAspect
+        monstres[indiceTemp] = MonsterCreate.GetComponent<MonsterScript>();
+
+    }
+
+    int GetRangeMin(int indice)
+    {
+        // TODO : Rajouter une fonction pour obtenir la valeur min de z
+        return 0;
+    }
+
+    int GetRangeMax(int indice)
+    {
+        // TODO : Rajouter une fonction pour obtenir la valeur max de z
+        return 0;
+    }
+
+    void SetCocktail()
+    {
+        
+    }
+    
+
+}
---
+++ Assets/Scripts/Class/MonsterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+    private int position;
+    private int attente = 2;
+    private int monsterAspect;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Position
+    {
+        get { return position; }
+        set { position = value; }
+    }
+
+    public int Attente
+    {
+        get { return attente; }
+        set { attente = value; }
+    }
+
+    public int MonsterAspect
+    {
+        get { return monsterAspect; }
+        set { monsterAspect = value; }
+    }
+
+    int youGotCocktail()
+    {
+        
+    }
+}
