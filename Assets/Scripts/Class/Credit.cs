510bd6d1d03a6ee5bbf213d78dc1b5f67956e7cb
Loïc <ljedi007@gmail.com>
2019-03-03 13:47:12 -0500

    Credits menu and how to play menu

---
+++ Assets/Scripts/Class/Credit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credit : MonoBehaviour
+{
+    public void ClickCreditButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/Class/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void ClickPlayButton()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void ClickQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void ClickHTPButtton()
+    {
+        SceneManager.LoadScene("HTP");
+    }
+
+    public void ClickCreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
